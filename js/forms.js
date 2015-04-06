bbb54ed2436092c6bf3a664cf572931afa7886d3
IMU-Drew <andrew-parker-1@uiowa.edu>
2015-04-06 15:46:24 -0500

    Added primitive mobile menu

--- js/forms.js
+++ js/forms.js
@@ -6,5 +6,28 @@ $(document).ready(function(){
     $("input[type='submit']").addClass('btn btn-default');
     $("input[type='reset']").addClass('btn btn-default');
 
+    $('#menuButton').click(function(){
+        if ($( ".navbar li:not(:first-child)" ).is( ":hidden" )) {
+            $( ".navbar li:not(:first-child)" ).slideDown(300);
+
+        } else {
+            $( ".navbar li:not(:first-child)" ).hide();
+
+        }});
+
+    $(window).on('resize', function(){
+
+
+        if($( ".navbar li:not(:first-child)" ).is( ":hidden" ) && $(window).width() > 768) {
+            // if the navigation menu and nav button are both hidden,
+            // then the responsive nav is closed and the nav menu is still hidden.
+            // just display the nav menu which will auto-hide at <560px width and remove class.
+            $( ".navbar li:not(:first-child)" ).show();
+        }
+    });
+
+
 
 });
+
+
