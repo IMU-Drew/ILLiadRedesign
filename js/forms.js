3ca064afecf0dad05e219fac78545a5a263eab7e
IMU-Drew <amparker8@ua.edu>
2015-04-07 09:33:47 -0500

    slideUp; menu styling on mobile; removed redundant selectors

--- js/forms.js
+++ js/forms.js
@@ -6,26 +6,26 @@ $(document).ready(function(){
     $("input[type='submit']").addClass('btn btn-default');
     $("input[type='reset']").addClass('btn btn-default');
 
+    var allButFirstChild = $( ".navbar li:not(:first-child)" );
     $('#menuButton').click(function(){
-        if ($( ".navbar li:not(:first-child)" ).is( ":hidden" )) {
-            $( ".navbar li:not(:first-child)" ).slideDown(300);
+        if (allButFirstChild.is( ":hidden" )) {
+            allButFirstChild.slideDown(300);
 
         } else {
-            $( ".navbar li:not(:first-child)" ).hide();
+            allButFirstChild.slideUp(300);
 
         }});
 
     $(window).on('resize', function(){
 
 
-        if($( ".navbar li:not(:first-child)" ).is( ":hidden" ) && $(window).width() > 768) {
+        if (allButFirstChild.is( ":hidden" ) && $(window).width() > 768) {
             // if the navigation menu and nav button are both hidden,
             // then the responsive nav is closed and the nav menu is still hidden.
             // just display the nav menu which will auto-hide at <560px width and remove class.
-            $( ".navbar li:not(:first-child)" ).show();
+           allButFirstChild.show();
         }
     });
-
 
 
 });
